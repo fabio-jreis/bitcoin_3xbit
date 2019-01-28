cccfa3bd1cc47448e7463898d089512ecd2f9832
Fabio Reis <fj.reis@gmail.com>
2019-01-28 21:16:24 -0200

    deploy

--- bitcoinapp/models.py
+++ bitcoinapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class BtcDeposit(object):
-    addr = ''
+class BtcDeposit(models.Model):
+    addr = models.CharField(max_length=40)
 
     @staticmethod
     def save(x):
