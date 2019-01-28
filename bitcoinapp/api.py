2f090c8ac650a8a68c0bbceb27a3749fe2b04874
Fabio Reis <fj.reis@gmail.com>
2019-01-28 13:53:20 -0200

    implementacoes passo a passo

--- bitcoinapp/api.py
+++ bitcoinapp/api.py
@@ -8,9 +8,10 @@ from blockcypher import simple_spend
 from django.shortcuts import render
 
 blockchainName = 'btc-testnet'
-#blockchainName = 'bcy'
 token = '6149e29abaeb46f0a778caf1b8a6db9b'
 _toSendSatoshis = 1
+
+_depositBTC = ""
 
 def getIp(request):
     response = requests.get('http://ip-api.com/json')
@@ -27,31 +28,29 @@ def get_hostname(request):
     texto = 'FABIO'
     return render(request, 'index.html', {'result': texto})
 
+def getDepositWallet(request):
+    global _depositBTC
+    return render(request, 'step2.html', {'gDepositBTC': _depositBTC})    
+
 
 def newWallet(request):
-
-    page = request.path_info
-    pageRet = 'index.html'
-    print(page)
-
     try:
+        print(_depositBTC)
         resp = generate_new_address(coin_symbol=blockchainName, api_key=token)
         assert is_valid_address(resp['address']), resp
 
         if resp:
             result = str(resp['address'])
-            print(str(resp))
+            global _depositBTC
+            _depositBTC = result
+            #print(str(resp))
         else:
             raise Exception('Ocorreu um erro, por favor tente novamente')
 
     except :
         result = 'Ocorreu um erro, por favor tente novamente'
 
-    if page=='/new_wallet_deposit/':
-        pageRet = 'step3.html'
-
-    print('pageRet' + pageRet)
-    return render(request, pageRet, {'result': result})
+    return render(request, 'index.html', {'result': result})
 
 def send_faucet(request):
     resp = send_faucet_coins(address_to_fund='C7rHYXAkuk93n2umpgmG96nrwDawyS2SC6', satoshis=100000, api_key=token, coin_symbol=blockchainName)
@@ -59,23 +58,42 @@ def send_faucet(request):
     return render(request, 'index.html')
     
 def addr_details(request):
-    addrObj = get_address_details('mtZFGURoy8HTqJXXGdR6jGPywM38e7aPLD', api_key=token, coin_symbol=blockchainName)
-    txrefs = addrObj['txrefs']
-    
-    lenn = len(txrefs)
-    print(lenn)
-    print(str(txrefs))
+    try:
+        global _depositBTC
+        _depositBTC = 'mtZFGURoy8HTqJXXGdR6jGPywM38e7aPLD'
+        #print('_depositBTC: ' + _depositBTC)
+        addrObj = get_address_details(_depositBTC, api_key=token, coin_symbol=blockchainName)
+        print('addrObj: ' + str(addrObj))
+        #txrefs = addrObj['txrefs']
+    except:
+        raise Exception('Ocorreu um erro, por favor tente novamente')
 
-    return render(request, 'index.html', {'txrefs': txrefs})
+    #print(str(txrefs))
+    return render(request, 'step3.html', {'addrObj': addrObj, 'gDepositBTC': _depositBTC})
+
+
+    #addrObj = get_address_details('mtZFGURoy8HTqJXXGdR6jGPywM38e7aPLD', api_key=token, coin_symbol=blockchainName)
+    #txrefs = addrObj['txrefs']
+    
+    #lenn = len(txrefs)
+    #print(lenn)
+    #print(str(txrefs))
+
+    #return render(request, 'index.html', {'txrefs': txrefs})
 
 def send_btc(request):
+
+    global _depositBTC
+    if _depositBTC == "":
+        return render(request, 'step2.html')
+
     tx_hash = simple_spend(
                     from_privkey='68950c093e49f888dcb90b0180e4e438d7eb13589f606d37ca1bd9e2f4b23903',
-                    to_address='mxiamccskZWRt8bZaMofFrETUqcJRE5aWn',
+                    to_address=_depositBTC,
                     to_satoshis=_toSendSatoshis,
                     privkey_is_compressed=True,
                     api_key=token,
                     coin_symbol=blockchainName)
     
     print(str(tx_hash))
-    return render(request, 'index.html', {'tx_hash_send': tx_hash})
+    return render(request, 'step2.html', {'tx_hash_send': tx_hash})
