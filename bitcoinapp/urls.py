13c005474ef23e92942116d58c2576b69fd23c15
Fabio Reis <fj.reis@gmail.com>
2019-01-28 22:02:17 -0200

    deploy

--- bitcoinapp/urls.py
+++ bitcoinapp/urls.py
@@ -2,20 +2,21 @@
 from django.conf.urls import url
 from bitcoinapp import views
 from bitcoinapp import api
+from django.views.decorators.cache import never_cache
 
 
 urlpatterns = [
-    url(r'^$', api.init, name='init'),
-    url(r'^new_wallet_deposit/$', api.newWallet),
-    url(r'^step_02/$', api.getDepositWallet),
-    url(r'^send_btc/$', api.send_btc),
-    url(r'^details_deposit/$', api.addr_details),
+    url(r'^$', never_cache(api.init), name='init'),
+    url(r'^new_wallet_deposit/$', never_cache(api.newWallet)),
+    url(r'^step_02/$', never_cache(api.getDepositWallet)),
+    url(r'^send_btc/$', never_cache(api.send_btc)),
+    url(r'^details_deposit/$', never_cache(api.addr_details)),
     
-    url(r'^new_wallet/$', api.newWallet),
+    url(r'^new_wallet/$', never_cache(api.newWallet)),
     #url(r'^step_02/$', views.step02PageView.as_view()),
     #url(r'^step_03/$', views.step03PageView.as_view()),
-    url(r'^faucet/$', api.send_faucet),
-    url(r'^details/$', api.addr_details)
+    url(r'^faucet/$', never_cache(api.send_faucet)),
+    url(r'^details/$', never_cache(api.addr_details))
     
     #url(r'^teste/$', api.teste, name='teste')
     #url(r'^$', views.HomePageView.as_view()),
