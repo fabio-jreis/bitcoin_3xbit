d01a3b5343f04e2c4ea0e847a66fc1d54c0017ac
Fabio Reis <fj.reis@gmail.com>
2019-01-29 00:54:28 -0200

    teste

--- bitcoinapp/api.py
+++ bitcoinapp/api.py
@@ -14,13 +14,16 @@ from .forms import ContactForm
 from django.http import HttpResponse
 
 from bitcoinapp.models import Deposits
+import random, string
 
 blockchainName = 'btc-testnet'
 token = settings.TOKEN
 privkey = settings.PRIVKEY
 _toSendSatoshis = 1
+sessionID = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
 
 def init(request):
+    print(sessionID)
 
     deposits = Deposits.objetos.all()
 
@@ -30,11 +33,13 @@ def init(request):
     return render(request, 'index.html')
 
 def getDepositWallet(request):
+    print(sessionID)
     deposit = Deposits.objetos.filter().first()
     return render(request, 'step2.html', {'gDepositBTC': deposit.address})    
 
 
 def newWallet(request):
+    print(sessionID)
     try:
         resp = generate_new_address(coin_symbol=blockchainName, api_key=token)
         assert is_valid_address(resp['address']), resp
@@ -59,6 +64,7 @@ def newWallet(request):
     return render(request, 'index.html', {'result': result})
     
 def addr_details(request):
+    print(sessionID)
     try:
         deposit = Deposits.objetos.filter().first()
         addrObj = get_address_details(deposit.address, api_key=token, coin_symbol=blockchainName)
@@ -68,6 +74,7 @@ def addr_details(request):
     return render(request, 'step3.html', {'addrObj': addrObj, 'gDepositBTC': deposit.address})
 
 def send_btc(request):
+    print(sessionID)
 
     email = request.GET["email"]
     deposit = Deposits.objetos.filter().first()
